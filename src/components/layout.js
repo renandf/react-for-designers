5b7d5225b702b34bab6e69677c2b58867cf31a4b
Renan Castro <renandf@gmail.com>
2022-02-06 15:00:26 +1100

    feat: Design Hero section

--- src/components/layout.js
+++ src/components/layout.js
@@ -25,25 +25,17 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
+      {/* <Header siteTitle={data.site.siteMetadata?.title || `Title`} /> */}
+      <main>{children}</main>
+      {/* <footer
         style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
+          marginTop: `2rem`,
         }}
       >
-        <main>{children}</main>
-        <footer
-          style={{
-            marginTop: `2rem`,
-          }}
-        >
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
-      </div>
+        © {new Date().getFullYear()}, Built with
+        {` `}
+        <a href="https://www.gatsbyjs.com">Gatsby</a>
+      </footer> */}
     </>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,23 +8,27 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Learn to design and code React apps</h1>
-    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
+    <div className="hero">
+      <div className="heroGroup">
+        <h1>Learn to design and code React apps</h1>
+        <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
+        <p>Now go build something great.</p>
+        {/* <StaticImage
+          src="../images/gatsby-astronaut.png"
+          width={300}
+          quality={95}
+          formats={["auto", "webp", "avif"]}
+          alt="A Gatsby astronaut"
+          style={{ marginBottom: `1.45rem` }}
+        /> */}
+        <p>
+          <Link to="/page-2/">Go to page 2</Link> <br />
+          {/* <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
+          <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
+          <Link to="/using-dsg">Go to "Using DSG"</Link> */}
+        </p>
+      </div>
+    </div>
   </Layout>
 )
 
